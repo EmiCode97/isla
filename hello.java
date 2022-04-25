05230fc4ca0763541994171f600cc73747afaf2d
EmiCode97 <87772887+EmiCode97@users.noreply.github.com>
2022-04-25 16:25:38 +0200

    isla

--- hello.java
+++ hello.java
@@ -24,3 +24,24 @@ jQuery(document).ready(function($) {
 
 
 <script sec="/main.js"></script>
+
+
+
+
+
+
+$(document).ready(function() {
+
+    // This is the pop-up gallery
+    $('[data-fancybox="gallery"]').fancybox({
+      protect: true,
+      keyboard: true,
+      arrows: true,
+      infobar: false,
+      buttons: [
+      "close"
+      ],
+    });
+  
+  });
+  
